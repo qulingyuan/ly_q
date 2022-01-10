b75e2ac217ea3602d03340452ce23c1fd0ee0b63
ly_qu <lingyuanqu@Gmail.com>
2022-01-10 20:32:53 +0800

    重新理解闭包

--- code/js-algorithm/sort/bubbleSort.js
+++ code/js-algorithm/sort/bubbleSort.js
@@ -1,14 +1,25 @@
 //冒泡排序 O(n^2)
+// Array.prototype.bubbleSort = function () {
+//   for (let i = 0; i < this.length - 1; i++) {
+//     for (let j = 0; j < this.length - 1 - i; j++) {
+//       if (this[j] > this[j + 1]) {
+//         const temp = this[j];
+//         this[j] = this[j + 1];
+//         this[j + 1] = temp;
+//       }
+//     }
+//   }
+// };
+
 Array.prototype.bubbleSort = function () {
   for (let i = 0; i < this.length - 1; i++) {
     for (let j = 0; j < this.length - 1 - i; j++) {
       if (this[j] > this[j + 1]) {
-        const temp = this[j];
-        this[j] = this[j + 1];
-        this[j + 1] = temp;
+        [this[j + 1], this[j]] = [this[j], this[j + 1]];
       }
     }
   }
 };
 const arr = [5, 4, 3, 2, 1];
 arr.bubbleSort();
+console.log(arr);
