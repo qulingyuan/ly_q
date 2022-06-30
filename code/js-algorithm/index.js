e31b22600d583b531a19c2cb98f1f2bb504fd98d
ly_qu <37199981+qulingyuan@users.noreply.github.com>
2022-06-30 20:55:16 +0800

    闭包

---
+++ code/js-algorithm/index.js
@@ -0,0 +1,19 @@
+function f() {
+  var name = " 极客时间 ";
+  let test1 = 1;
+  const test2 = 2;
+  var innerBar = {
+    setName: function (value) {
+      name = value;
+    },
+    getName: function () {
+      console.log(test1);
+      return name;
+    },
+  };
+  return innerBar;
+}
+var bar = f();
+bar.setName(" 极客邦 ");
+bar.getName();
+console.log(bar.getName());
